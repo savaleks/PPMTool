be9f1256bcb381088a44e04c18ee3c64e43acd48
Alexander <savaleks@gmail.com>
2019-08-30 22:24:44 +0300

    Added Validation

--- PPMTFullStack/src/main/java/com/savaleks/ppmtool/domain/Project.java
+++ PPMTFullStack/src/main/java/com/savaleks/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package com.savaleks.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,14 +13,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 or 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project(){}
--- PPMTFullStack/src/main/java/com/savaleks/ppmtool/web/ProjectController.java
+++ PPMTFullStack/src/main/java/com/savaleks/ppmtool/web/ProjectController.java
@@ -2,13 +2,21 @@ package com.savaleks.ppmtool.web;
 
 import com.savaleks.ppmtool.domain.Project;
 import com.savaleks.ppmtool.services.ProjectService;
+import com.savaleks.ppmtool.services.ValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,8 +25,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private ValidationErrorService  validationErrorService;
+
     @PostMapping
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap = validationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
+
         Project project1 = projectService.savedOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
